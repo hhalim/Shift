524d33d90fe6d635a0f8cfc11f7fa64084220158
Hartono Halim <gopher@equalbyte.com>
2017-01-27 16:18:04 -0600

    Added missing file.

---
+++ Shift/RegisterAssembly.cs
@@ -0,0 +1,27 @@
+﻿using Autofac;
+using Shift.DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shift
+{
+    public static class RegisterAssembly
+    {
+        public static void RegisterTypes(ContainerBuilder builder, Options options)
+        {
+            var parameters = Helpers.GenerateNamedParameters(new Dictionary<string, object> { { "connectionString", options.DBConnectionString }, { "encryptionKey", options.EncryptionKey } });
+            if (options.UseCache)
+            {
+                builder.RegisterType<DataLayer.Redis.Cache>().As<IJobCache>().WithParameter("configurationString", options.CacheConfigurationString);
+                builder.RegisterType<JobDAL>().As<JobDAL>().WithParameters(parameters);
+            }
+            else
+            {
+                builder.RegisterType<JobDAL>().As<JobDAL>().UsingConstructor(typeof(string), typeof(string)).WithParameters(parameters);
+            }
+        }
+    }
+}
